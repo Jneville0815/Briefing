3680f30ce2aca57bb4afefaef75072b7e33b5d07
Jimmy Neville <jimmyeneville@gmail.com>
2026-04-23 12:08:50 -0400

    Initial Commit

---
+++ Briefing/BriefingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BriefingApp.swift
+//  Briefing
+//
+//  Created by Jimmy Neville on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct BriefingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
